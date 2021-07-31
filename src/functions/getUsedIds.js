304e0fa9e473b4d9289de2d34f2ea76d8c24cab4
tomdmitchell <thomasdavymitchell@gmail.com>
2021-07-31 16:11:05 +0100

    adding error handling & logs

--- src/functions/getUsedIds.js
+++ src/functions/getUsedIds.js
@@ -1,8 +1,12 @@
 import fsPromises from 'fs/promises';
 
 const getUsedIds = async (existingUserId) => {
-  const existingFile = await fsPromises.readFile(`src/temp_user_data/${existingUserId}.json`);
-  return JSON.parse(existingFile).usedIds;
+  try {
+    const existingFile = await fsPromises.readFile(`src/temp_user_data/${existingUserId}.json`);
+    return JSON.parse(existingFile).usedIds;
+  } catch (err) {
+    console.log(`ERROR getting UsedIds: ${err}`)
+  }
 };
 
 module.exports = { getUsedIds };
--- src/functions/handleExpiredFiles.js
+++ src/functions/handleExpiredFiles.js
@@ -7,15 +7,20 @@ const handleExpiredFiles = async () => {
 };
 
 const deleteExpiredFiles = async () => {
-  const allFileNames = await fsPromises.readdir(`src/temp_user_data/`);
-  for (let i = 0; i < allFileNames.length; i++) {
-    const file = await fsPromises.readFile(`src/temp_user_data/${allFileNames[i]}`);
-    const expiry = JSON.parse(file).expiry;
-    if (new Date().getTime() > expiry) {
-      await fsPromises.unlink(`src/temp_user_data/${allFileNames[i]}`);
-    } else {
-      console.log(`${allFileNames[i]} not expired`);
+  try {
+    const allFileNames = await fsPromises.readdir(`src/temp_user_data/`);
+    for (let i = 0; i < allFileNames.length; i++) {
+      const file = await fsPromises.readFile(`src/temp_user_data/${allFileNames[i]}`);
+      const expiry = JSON.parse(file).expiry;
+      if (new Date().getTime() > expiry) {
+        await fsPromises.unlink(`src/temp_user_data/${allFileNames[i]}`);
+        console.log(`Deleted expired file: ${allFileNames[i]}`);
+      } else {
+        console.log(`${allFileNames[i]} not expired`);
+      }
     }
+  } catch (err) {
+    console.log(`ERROR handling expired files: ${err}`);
   }
 };
 
