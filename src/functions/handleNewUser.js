0b5e0be2d66e3e3a9f334474e5e26dc894d0fe7e
tomdmitchell <thomasdavymitchell@gmail.com>
2021-07-31 12:31:14 +0100

    increasing cookie expiry time

--- src/functions/handleNewUser.js
+++ src/functions/handleNewUser.js
@@ -1,7 +1,7 @@
 import fsPromises from 'fs/promises';
 
 const handleNewUser = async (newUserId, releaseIds) => {
-  const fileExpiry = new Date().getTime() + 3600000;
+  const fileExpiry = new Date().getTime() + 7200000;
   const jsonData = JSON.stringify({ expiry: fileExpiry, usedIds: releaseIds }, null, 2);
   try {
     await fsPromises.writeFile(`src/temp_user_data/${newUserId}.json`, jsonData);
