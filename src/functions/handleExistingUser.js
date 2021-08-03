02f896d48b98e44bf12154c812b2033c1ff4f031
tomdmitchell <thomasdavymitchell@gmail.com>
2021-08-03 22:47:13 +0100

    correcting handleExistingUser

--- src/functions/handleExistingUser.js
+++ src/functions/handleExistingUser.js
@@ -3,9 +3,13 @@ import fsPromises from 'fs/promises';
 const handleExistingUser = async (existingUserId, newUniqueIds) => {
   try {
     const existingFile = await fsPromises.readFile(`src/temp_user_data/${existingUserId}.json`);
-    const existingUsedIdsArr = JSON.parse(existingFile).usedIds;
-    const updatedIdArr = existingUsedIdsArr.concat(newUniqueIds);
-    const updatedJsonData = JSON.stringify({usedIds: updatedIdArr}, null, 2);
+    const existingFileParsed = JSON.parse(existingFile);
+    const updatedIdArr = existingFileParsed.usedIds.concat(newUniqueIds);
+    const updatedJsonData = JSON.stringify(
+      { expiry: existingFileParsed.expiry, usedIds: updatedIdArr },
+      null,
+      2
+    );
     await fsPromises.writeFile(`src/temp_user_data/${existingUserId}.json`, updatedJsonData);
   } catch (err) {
     console.log(`ERROR writing JSON in handleExistingUser: ${err}`);
