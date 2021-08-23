02fc15aff929ae48126f9d338c034d2ccb768632
tomdmitchell <thomasdavymitchell@gmail.com>
2021-08-23 11:59:24 +0100

    add batch param validation

--- src/functions/handleBatchNumber.js
+++ src/functions/handleBatchNumber.js
@@ -1,5 +1,9 @@
 const handleBatchNumber = (batchParam) => {
-  if (Number(batchParam) < 1 || Number(batchParam) > 15) {
+  if (!batchParam) {
+    throw 'ERROR: NO BATCH PARAM EXISTS';
+  } else if (isNaN(batchParam)) {
+    throw 'ERROR: BATCH PARAM NaN';
+  } else if (Number(batchParam) < 1 || Number(batchParam) > 15) {
     throw 'ERROR: BATCH REQ EXCEEDS LIMIT';
   } else {
     return Number(batchParam);
