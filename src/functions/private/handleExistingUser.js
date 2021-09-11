2b94299811e5e4fdf69af67b53d8039baa4a59e8
tomdmitchell <thomasdavymitchell@gmail.com>
2021-09-11 08:48:28 +0100

    adding logs

--- src/functions/private/handleExistingUser.js
+++ src/functions/private/handleExistingUser.js
@@ -4,12 +4,15 @@ const handleExistingUser = async (existingUserId, newUniqueIds) => {
   try {
     const existingFile = await fsPromises.readFile(`src/temp_user_data/${existingUserId}.json`);
     const existingFileParsed = JSON.parse(existingFile);
+    console.log('existing file parsed: ', existingFileParsed);
     const updatedIdArr = existingFileParsed.usedIds.concat(newUniqueIds);
+    console.log('updated id arr: ', updatedIdArr);
     const updatedJsonData = JSON.stringify(
       { expiry: existingFileParsed.expiry, usedIds: updatedIdArr },
       null,
       2
     );
+    console.log('updated json data: ', updatedJsonData);
     await fsPromises.writeFile(`src/temp_user_data/${existingUserId}.json`, updatedJsonData);
   } catch (err) {
     console.log(`ERROR writing JSON in handleExistingUser: ${err}`);
